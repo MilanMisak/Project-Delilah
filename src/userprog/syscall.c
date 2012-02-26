57f11811714cf830598a4a2ff4038c005266e834
Milan Misak <milan.misak10@imperial.ac.uk>
2012-02-26 16:25:58 +0000

    Basic syscall handler

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -6,6 +6,16 @@
 
 static void syscall_handler (struct intr_frame *);
 
+static int get_integer_argument(int n, void *esp);
+static void *get_pointer_argument(int n, void * esp);
+
+static void h_halt (void *esp);
+static void h_exit (void *esp);
+static void h_exec (void *esp);
+
+/* System call handlers array. */
+static void (*handlers[3]) (void *esp) = {*h_halt, *h_exit, *h_exec};
+
 void
 syscall_init (void) 
 {
@@ -13,8 +23,67 @@ syscall_init (void)
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f) 
 {
   printf ("system call!\n");
+
+  /* Get ESP from the interrupt frame. */
+  void *esp = f->esp;
+  
+  /* Get the system call number from the stack. */
+  int syscall_number;
+  asm ("movl %1, %0" : "=m" (syscall_number) : "r" (esp));
+
+  /* Call the system call handler. */
+  (*handlers[syscall_number]) (esp);
+}
+
+static int
+get_integer_argument(int n, void *esp)
+{
+  int arg;
+  //TODO - remove or make work
+  //asm ("movl %1(%2), %0" : "=r" (arg) : "i" (n), "r" (e));
+  asm ("movl %1, %0" : "=r" (arg) : "r" (esp + 4 * n));
+  return arg;
+}
+
+static void
+*get_pointer_argument(int n, void *esp)
+{
+  void *arg;
+  asm ("movl %1, %0" : "=r" (arg) : "r" (esp + 4 * n));
+  return arg;
+}
+
+/* The halt system call handler. */
+static void
+h_halt (void *esp)
+{
+  //TODO - halt SC
+  thread_exit ();
+}
+
+/* The exit system call handler. */
+static void
+h_exit (void *esp)
+{
+  int status = get_integer_argument (1, esp);
+
+  //TODO - exit SC
+  //asm ("movl status, %eax");
+  asm ("movl %0, %%eax" : : "a" (status));
+  //asm ("movl %0, %eax" : : "i" (status) : "eax");
+
+  thread_exit ();
+}
+
+/* The exec system call handler. */
+static void
+h_exec (void *esp)
+{
+  char *cmd_line = get_pointer_argument (1, esp);
+
+  //TODO - exec SC
   thread_exit ();
 }
