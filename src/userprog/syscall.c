93deb5faeff1cd045ee38f50b021cf7049b8ae1f
Jack Bracewell <jb2910@vm-shell4.doc.ic.ac.uk>
2012-02-27 14:07:30 +0000

    Pointer_is_mapped() boolean included for safe memory access.

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -1,4 +1,5 @@
 #include "userprog/syscall.h"
+#include "userprog/pagedir.h"
 #include <stdio.h>
 #include <syscall-nr.h>
 #include "devices/shutdown.h"
@@ -75,29 +76,31 @@ get_integer_argument (int n, void *esp)
   UADDR must be below PHYS_BASE.
   Returns the byte value if successful, -1 if a segfault
   occurred. */
-static int
+/* static int
 get_user (const uint8_t *uaddr) {
   int result;
   asm ("movl $1f, %0; movzbl %1, %0; 1:"
        : "=&a" (result) : "m" (*uaddr));
   return result;
-}
+} */
 
 /* Writes BYTE to user address UDST.
    UDST must be below PHYS_BASE.
    Returns true if successful, false if a segfault occured. */
-static bool
+/*static bool
 put_user (uint8_t *udst, uint8_t byte) {
   int error_code;
   asm ("movl $1f, %0; movb %b2, %1; 1:"
       : "=&a" (error_code), "=m" (*udst) : "q" (byte));
   return error_code != -1;
-}
+}*/
 
 static bool
-pointer_is_usable (uint8_t *udst) {
-    return (udst != NULL && *udst != NULL && is_user_vaddr (*udst));
+pointer_is_mapped (uint8_t *udst) {
+  return (udst != NULL && is_user_vaddr(*udst) &&
+          pagedir_get_page (active_pd (), *udst) != NULL);
 }
+
 
 /* The halt system call handler. */
 static void
