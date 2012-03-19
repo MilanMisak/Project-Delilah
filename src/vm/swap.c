9f6cac229b8c5427823c9eac17c594dfbdc12f9c
Milan Misak <milan.misak10@imperial.ac.uk>
2012-03-19 12:03:57 +0000

    Swap table

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -1,12 +1,23 @@
 #include "swap.h"
 #include <bitmap.h>
 #include "devices/block.h"
+#include "threads/malloc.h"
+#include "threads/vaddr.h"
 
+#define ENTRY_COUNT (BLOCK_SECTOR_SIZE * \
+                       block_size (block_get_role (BLOCK_SWAP)) / \
+                       PGSIZE)
+
+/* Bitmap to record which indices in swap table are busy. */
 static struct bitmap *used_map;
+
+static struct swap_table_entry **swap_table;
 
 /* Initializes the swap table. */
 void
 swap_init (void)
 {
-  used_map = bitmap_create (BLOCK_SECTOR_SIZE * block_size (block_get_role (BLOCK_SWAP)));
+  used_map = bitmap_create (ENTRY_COUNT);
+  swap_table = (struct swap_table_entry **)
+                   malloc (ENTRY_COUNT * sizeof (struct swap_table_entry));
 }
--- src/vm/swap.h
+++ src/vm/swap.h
@@ -1,6 +1,13 @@
 #ifndef VM_SWAP_H
 #define VM_SWAP_H
 
+/* An entry for the swap table. */
+struct swap_table_entry
+  {
+    int start_sector_index;   /* Index of a sector at which entry starts. */
+    int offset;               /* Offset inside the sector. */
+  };
+
 void swap_init (void);
 
 #endif
