206db17153c18058b9e10d6faa04908b46952f1b
Milan Misak <milan.misak10@imperial.ac.uk>
2012-03-22 11:45:01 +0000

    Almost nothing changed

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -154,7 +154,6 @@ start_process (void *args_)
   if_.esp -= 4;
   *((int *) if_.esp) = 0;
 
-
   /* Push pointers to ARGV elements on stack in reverse order. */
   for (i = argc - 1; i >= 0; i--)
     {
@@ -545,7 +544,7 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
       if (page == NULL)
         {
           //TODO - do something here?
-          //printf ("baaad");
+          printf ("baaad");
           return false;
         }
 
