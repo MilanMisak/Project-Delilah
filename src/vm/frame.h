f81253204bfd539a42a84a7de2e48a129e87da43
Craig Ellis <cellis_222@hotmail.com>
2012-03-23 20:17:17 +0000

    a little tidying

--- src/vm/frame.h
+++ src/vm/frame.h
@@ -27,7 +27,7 @@ void frame_insert (void *faddr, void *uaddr, bool write);
 /* Removes and returns a frame from the frame table. */
 struct frame* frame_remove (void *);
 
-//TODO - comment
+/* Removes the frame containing the given user virtual address. */
 void frame_remove_by_upage (void *upage);
 
 /* Hash function for frames. */
@@ -39,13 +39,6 @@ bool frame_less_func (const struct hash_elem *a, const struct hash_elem *b,
 
 /* Evicts a frame randomly, creates a page and sends it to swap */
 void frame_evict (void);
-
-/* Destructor for a frame. */
-void frame_destroy (struct hash_elem *, void *);
-
-/* Destroys and frees the frame table. */
-//TODO: call on exit, or just move this code (it's like a line long)
-void frame_table_destroy (void);
 
 /* Finds a frame, given a user virtual address */
 struct frame *frame_find_upage (uint8_t *);
